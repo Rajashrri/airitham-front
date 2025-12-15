109c79df3b37530af1c62222ec6686ed2148453d
Yogesh-DigiHost <yogesh.sankpal@digihost.in>
2025-12-15 17:20:53 +0530

    dash

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,64 +1,61 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
-
-//Import Breadcrumb
-import Breadcrumbs from '../../components/Common/Breadcrumb';
-
-//Import Components
+import Breadcrumbs from "../../components/Common/Breadcrumb";
 import MiniWidgets from "./MiniWidgets";
-import RevenueAnalytics from "./RevenueAnalytics";
-import SalesAnalytics from "./SalesAnalytics";
-import EarningReports from "./EarningReports";
-import Sources from "./Sources";
-import RecentlyActivity from "./RecentlyActivity";
-import RevenueByLocations from "./RevenueByLocations";
-import ChatBox from "./ChatBox";
-import LatestTransactions from "./LatestTransactions";
 
 class Dashboard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            breadcrumbItems : [
-                { title : "AIRITHM", link : "/" },
-                { title : "Dashboard", link : "#" },
-            ],
-            reports : [
-                { icon : "ri-stack-line", title : "Blogs", value : "1452", rate: "", desc   : "Total Blogs" },
-                { icon : "ri-store-2-line", title : "Contact Enquiries", value   : "60" ,rate: "", desc   : "Lifetime" },
-                { icon : "ri-briefcase-4-line", title : "Contact Enquiries", value : "$ 15.4", rate : "", desc : "This Month" },
-                // { icon : "ri-briefcase-4-line", title : "Average Price", value : "$ 15.4", rate : "2.4%", desc : "From previous period" },
-            ]
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      breadcrumbItems: [
+        { title: "AIRITHM", link: "/" },
+        { title: "Dashboard", link: "#" },
+      ],
+      reports: [
+        { icon: "ri-stack-line", title: "Blogs", value: "0", desc: "Total Blogs" },
+        { icon: "ri-store-2-line", title: "Contact Enquiries", value: "70", desc: "Lifetime" },
+        { icon: "ri-briefcase-4-line", title: "This Month", value: "$0", desc: "Revenue" },
+      ],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchDashboardCounts();
+  }
+
+  fetchDashboardCounts = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/dashboard/counts`);
+      const data = await response.json();
+
+      this.setState((prevState) => ({
+        reports: prevState.reports.map((report, index) => {
+          if (index === 0) return { ...report, value: data.totalBlogs };
+          if (index === 1) return { ...report, value: data.totalContacts };
+          return report;
+        }),
+      }));
+    } catch (error) {
+      console.error("Error fetching dashboard counts:", error);
     }
+  };
 
-    render() {
-        return (
-            <React.Fragment>
-                <div className="page-content">
-                    <Container fluid>
-
-                    <Breadcrumbs title="Dashboard" breadcrumbItems={this.state.breadcrumbItems} />
-                        <Row>
-                            <Col xl={12}>
-                                <Row>
-                                    <MiniWidgets reports={this.state.reports} />
-                                </Row>
-                                {/* revenue Analytics */}
-                                {/* <RevenueAnalytics /> */}
-                            </Col>
-
-                          
-                        </Row>
-                        
-                        
-                      
-
-                    </Container> 
-                </div>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <div className="page-content">
+        <Container fluid>
+          <Breadcrumbs title="Dashboard" breadcrumbItems={this.state.breadcrumbItems} />
+          <Row>
+            <Col xl={12}>
+              <Row>
+                <MiniWidgets reports={this.state.reports} />
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
